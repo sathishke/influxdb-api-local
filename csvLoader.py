540cb07ee3e139ddde0cad67aa830233e84a7d99
sathishke <43739573+sathishke@users.noreply.github.com>
2020-04-02 18:55:30 +0530

    config and log added

--- csvLoader.py
+++ csvLoader.py
@@ -1,6 +1,14 @@
 from influxdb import InfluxDBClient
 import csv
+import logging
 import subprocess
+from time import time
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import logger_config
+
+
+logger = logging.getLogger(__name__)
 
 default_target_client = InfluxDBClient(host='localhost', port='8086', database='preprocessed')
 
@@ -17,7 +25,8 @@ def CsvToInfluxDB(fileName, influxdbClient=default_target_client, customInputTim
         4th column = timestamp read from influxdb
         5th column = custom timestamp
     """
-    print("Pushing data from", fileName, "into", influxdbClient._baseurl)
+    ts = time()
+
     with open(fileName) as inputFile:
         reader = csv.reader(inputFile, delimiter='~')
         for row in reader:
@@ -31,21 +40,33 @@ def CsvToInfluxDB(fileName, influxdbClient=default_target_client, customInputTim
                 line_data = measurement + ',' + tags + ' ' + fields + ' ' + customtimestamp
             #print(line_data)
             influxdbClient.write_points(line_data, protocol="line",batch_size=100)
+    logger.info("Pushed data from %s into %s in %s seconds", fileName, influxdbClient._baseurl, time() - ts)
 
 #influx -database test1 -host localhost -execute SELECT * FROM Ax WHERE time > now() - 24h -format csv > E:/influxdb-api-local/sample_extract.csv
 def influxDBToCsv(fileName, query, host="localhost", port="8086", database="test1", influxClientPath="."):
     """Get data from influxdb to csvLoade
     Executes above command in terminal, make sure you have influx client installed
     """
-    print("Loading data from influxdb http(s)://"+ host+ ":"+ port, "into", fileName)
+    ts = time()
     with open(fileName, "w") as file:
         subprocess.run([influxClientPath + "/influx", "-database", database, "-host", host, "-port",\
         port, "-execute", query, "-format", "csv"], stdout=file)
-        
-#pull data from influxdb and store in csv
-#influxDBToCsv("../raw/test.csv", query="SELECT * FROM Ax WHERE time > now() - 24h", influxClientPath="/Users/aj_heartnett/PycharmProjects/Influxdb_py_main/influxdb-1.7.10-1/usr/bin")
-#influxDBToCsv("./raw/test.csv", query="SELECT * FROM Ax WHERE time > now() - 24h", influxClientPath="E:\influxdb-1.7.10-1")
-
-#pull data from csv and store in influxdb
-#CsvToInfluxDB('sample_data.csv', InfluxDBClient(host='localhost', port='8086', database='preprocessed'), time.time_ns())
-#CsvToInfluxDB('sample_data_test1.csv', InfluxDBClient(host='localhost', port='8086', database='preprocessed'), time.time_ns())
+    logger.info("Loaded data from influxdb http(s)://%s:%s into %s in %s seconds",host, port, fileName, time() - ts)
+    
+if __name__ == '__main__':
+    logger_config.setup_logging()    
+    #filename='myapp.log'
+    #pull data from influxdb and store in csv
+    #influxDBToCsv("../raw/test.csv", query="SELECT * FROM Ax WHERE time > now() - 24h", influxClientPath="/Users/aj_heartnett/PycharmProjects/Influxdb_py_main/influxdb-1.7.10-1/usr/bin")
+    #influxDBToCsv("./raw/test.csv", query="SELECT * FROM Ax WHERE time > now() - 24h", influxClientPath="E:\influxdb-1.7.10-1")
+    scheduler = BlockingScheduler()
+    kwArgs = {"fileName": "./raw/test.csv", "query": "SELECT * FROM Ax WHERE time > now() - 24h", "influxClientPath": "E:\influxdb-1.7.10-1"}
+    logger.debug(kwArgs)
+    scheduler.add_job(influxDBToCsv, 'interval', kwargs=kwArgs,seconds=5)
+    try:
+        scheduler.start()
+    finally:
+        scheduler.shutdown()
+    #pull data from csv and store in influxdb
+    #CsvToInfluxDB('sample_data.csv', InfluxDBClient(host='localhost', port='8086', database='preprocessed'), time.time_ns())
+    #CsvToInfluxDB('sample_data_test1.csv', InfluxDBClient(host='localhost', port='8086', database='preprocessed'), time.time_ns())
--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,24 @@
 import csvLoader
 import time
 import csv
-from datetime import datetime
 from influxdb import InfluxDBClient
 import configparser
+import logging
+from pytz import utc
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor
+import logger_config
 
 config = configparser.ConfigParser()
-config.read('./config/config.ini')
+config.read('./config/application.ini')
 influxdb_config = config['influxdb']
-default_config = config['DEFAULT']
+default_config = config['common']
+queries_config = config['queries']
+
+logger = logging.getLogger(__name__)
 
 def preprocess(inputfileName, outputFileName, start_time=None):
     """
@@ -17,7 +27,7 @@ def preprocess(inputfileName, outputFileName, start_time=None):
     write to outputfile
     starting time of process will stored in outputfile
     """
-    print("Preprocessing", inputfileName, "started @", datetime.now())
+    ts = time.time()
     with open(inputfileName) as inputFile:
         with open(outputFileName, 'w') as outputFile:
             reader = csv.reader(inputFile, delimiter=',')
@@ -36,15 +46,18 @@ def preprocess(inputfileName, outputFileName, start_time=None):
                     customtimestamp = row[1] if start_time == None else str(start_time)
                     line_data = measurement + '~' + tags + "~" + fieldNames+ "=" + fields + '~' + customtimestamp
                     outputFile.write(line_data + '\n')
+    logger.info("Preprocessed %s in %s seconds", inputfileName, time.time() - ts)
+                
 
 preprocessOutputDB = InfluxDBClient(host=influxdb_config['preprocessed_data_host'],
                                     port=influxdb_config['preprocessed_data_port'],
                                     database=influxdb_config['preprocessed_database'])
 
-def loadAndPreprocess(query, query_interval, rawDatahost="localhost", rawDataport='port', rawDatabase="test1",
+def loadAndPreprocess(query, rawDatahost="localhost", rawDataport='port', rawDatabase="test1",
                       influxClientPath=".", inputFilePath = ".", outputFilePath = "./", fileName=None, preprocessOutputDB=preprocessOutputDB):
     """Starting point for preprocess
     """
+    ts = time.time()
     start_time = time.time_ns()
     standardFileName = "/" + rawDatahost + "_" + rawDataport + "_" + rawDatabase + "_" + str(start_time)
     if fileName is None:
@@ -57,14 +70,36 @@ def loadAndPreprocess(query, query_interval, rawDatahost="localhost", rawDatapor
     preprocess(inputFileName, outputFileName)
     #write data
     csvLoader.CsvToInfluxDB(outputFileName, preprocessOutputDB)
-    print('Waiting.....')
-    time.sleep(query_interval)
+    logger.info("Loaded, preprocessed & pushed %s in %s seconds", inputFileName, time.time() - ts)
 
-#sleep for 30 secs
-query_interval=int(default_config['query_interval'])
 
-while "true":
-    loadAndPreprocess(default_config['query'], query_interval, influxdb_config['raw_data_host'],influxdb_config['raw_data_port'],
-                      influxdb_config['raw_database'],influxdb_config['client_path'],
-                      inputFilePath = "./raw", outputFilePath = "./preprocessed", preprocessOutputDB=preprocessOutputDB)
-#preprocess("test.csv", time.time_ns())
+if __name__ == '__main__':
+    logger_config.setup_logging()    
+    jobstores = {
+        'default': MemoryJobStore()
+    }
+    executors = {
+        'default': ThreadPoolExecutor(20)
+    }
+    job_defaults = {
+        'coalesce': False,
+        'max_instances': 1
+    }
+    logger.info("Starting preprocessor")
+    scheduler = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+    for query_key in queries_config:
+        kwArgs = {"query": queries_config[query_key], "rawDatahost" : influxdb_config['raw_data_host'], "rawDataport" : influxdb_config['raw_data_port'],
+              "rawDatabase" : influxdb_config['raw_database'], "influxClientPath" : influxdb_config['client_path'],
+              "inputFilePath" :default_config['raw_file_path'], "outputFilePath" : default_config['preprocessed_file_path'],
+              "preprocessOutputDB" : preprocessOutputDB}
+        logger.debug(kwArgs)
+        logger.info("Adding job to preprocess %s", query_key)
+        scheduler.add_job(loadAndPreprocess, 'interval', name=query_key, kwargs=kwArgs,seconds=int(default_config['query_interval']))
+    try:
+        scheduler.start()
+    finally:
+        scheduler.shutdown()
+    #loadAndPreprocess(default_config['query'], influxdb_config['raw_data_host'],influxdb_config['raw_data_port'],
+    #                  influxdb_config['raw_database'],influxdb_config['client_path'],
+    #                  inputFilePath = "./raw", outputFilePath = "./preprocessed", preprocessOutputDB=preprocessOutputDB)
+    #preprocess("test.csv", time.time_ns())
