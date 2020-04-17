32ebd352c31aa967faa1b1adf27c72cc39350b20
sathishke <43739573+sathishke@users.noreply.github.com>
2020-04-17 16:40:54 +0530

    confidence interval - changed to .95 in prediction measurement

--- prediction_accuracy.py
+++ prediction_accuracy.py
@@ -17,31 +17,31 @@ config = ConfigParser(interpolation=ExtendedInterpolation())
 config.read('./config/application.ini')
 influxdb_config = config['influxdb']
 
-preprocess_db = DataFrameClient(host=influxdb_config['preprocessed_data_host'],
-                                    port=influxdb_config['preprocessed_data_port'],
-                                    database=influxdb_config['preprocessed_database'])
+preprocess_db = DataFrameClient(host=influxdb_config['forecast_db_host'],
+                                    port=influxdb_config['forecast_db_port'],
+                                    database=influxdb_config['forecast_db'])
 
 def measure_accuracy(input_db = preprocess_db):
     
-    print("Starting prediction")
     #get latest 300 points from last 24 hours of data and measure accuracy
     res = input_db.query('select * from Ax where time > now()- 24h order by time desc limit 300')
-    measurement = next(iter(res))
-    ret = res[measurement]
-    df = pd.DataFrame.from_dict(ret)
-    df['ds'] =df.index.astype(str).str[:-6]
-    df['y'] = df['value']
-    print(df)
-    
-    m = Prophet()
-    m.fit(df)
-    df_cv = cross_validation(m, period='1 seconds', horizon = '60 seconds')
-    print(df_cv)
-    df_p = performance_metrics(df_cv)
-    df_p.head()
-    #plot mean squared error
-    #refer https://facebook.github.io/prophet/docs/diagnostics.html
-    plot_cross_validation_metric(df_cv, metric='mse')
-    
+    for measurement in res:
+        print("Measuring prediction for " + measurement)
+        ret = res[measurement]
+        df = pd.DataFrame.from_dict(ret)
+        df['ds'] =df.index.astype(str).str[:-6]
+        df['y'] = df['value']
+        print(df)
+        
+        m = Prophet(interval_width=0.95)
+        m.fit(df)
+        df_cv = cross_validation(m, period='1 seconds', horizon = '60 seconds')
+        print(df_cv)
+        df_p = performance_metrics(df_cv)
+        df_p.head()
+        #plot mean squared error
+        #refer https://facebook.github.io/prophet/docs/diagnostics.html
+        plot_cross_validation_metric(df_cv, metric='mse')
+    print("Done")
 if __name__ == '__main__':
     measure_accuracy(preprocess_db)
